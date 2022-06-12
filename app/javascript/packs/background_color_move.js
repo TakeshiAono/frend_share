f6849928bbb4f7202ec849b1ef572c2f4f2bd16f
aono <takeshi.aono001@gmail.com>
2022-06-12 22:41:47 +0900

    .js反映完了

---
+++ app/javascript/packs/background_color_move.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $(".recode_1").css("background-color","rgba(0, 185, 241, 0.527)");
+});
+console.log("aono");
---
+++ app/javascript/packs/greet.js
@@ -0,0 +1,6 @@
+if (window.name != "currentwindow")
+	{
+    alert("こんにちは!!今日も良い一日を!!")
+		location.reload();
+		window.name = "currentwindow";
+	}
